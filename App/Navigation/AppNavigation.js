f6b4bb8b3439b23acbd2299b41c6a2880f37db95
jQN <jqn@jQNs-MacBook-Pro.local>
2019-09-30 17:20:06 +0200

    feature: react native camera mlkit integration

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,18 +1,23 @@
-import { createStackNavigator, createAppContainer } from 'react-navigation'
-import LaunchScreen from '../Containers/LaunchScreen'
+import { createStackNavigator, createAppContainer } from "react-navigation";
+import LaunchScreen from "../Containers/LaunchScreen";
+import MlKitScanner from "../Containers/MlKitScanner";
 
-import styles from './Styles/NavigationStyles'
+import styles from "./Styles/NavigationStyles";
 
 // Manifest of possible screens
-const PrimaryNav = createStackNavigator({
-  LaunchScreen: { screen: LaunchScreen }
-}, {
-  // Default config for all screens
-  headerMode: 'none',
-  initialRouteName: 'LaunchScreen',
-  navigationOptions: {
-    headerStyle: styles.header
-  }
-})
+const PrimaryNav = createStackNavigator(
+  {
+    LaunchScreen: { screen: LaunchScreen },
+    MlKit: { screen: MlKitScanner },
+  },
+  {
+    // Default config for all screens
+    headerMode: "none",
+    initialRouteName: "MlKit",
+    navigationOptions: {
+      headerStyle: styles.header,
+    },
+  },
+);
 
-export default createAppContainer(PrimaryNav)
+export default createAppContainer(PrimaryNav);
--- ios/ReactNativeCognexScanner/AppDelegate.m
+++ ios/ReactNativeCognexScanner/AppDelegate.m
@@ -11,10 +11,14 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+#import <Firebase.h>
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [FIRApp configure];
+  
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"ReactNativeCognexScanner"
